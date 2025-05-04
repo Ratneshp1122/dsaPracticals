b96c32162d39bd38e1483d4194ceb5376b0a8729
Ratnesh Patnaik <141709830+Ratneshp1122@users.noreply.github.com>
2025-05-04 23:19:36 +0530

    Update 1. ADTFunctions.py

--- 1. ADTFunctions.py
+++ 1. ADTFunctions.py
@@ -1,34 +1,83 @@
-class adt:
+class ADT:
+    def __init__(self): # Constructor
+        self.a = []
 
-  def __init__(self):
-    self.a = set()
+    def add(self, x):
+        for item in self.a:
+            if item == x:
+                return 
+        self.a.append(x)
 
-  def add(self,x):
-    self.a.add(x)
+    def remove(self, x):
+        found = False
+        for i in range(len(self.a)):
+            if self.a[i] == x:
+                del self.a[i]
+                found = True
+                break
+        if not found:
+            print(f"Element {x} not found in Set.")
 
-  def remove(self,x):
-    if(x in self.a):
-        self.a.remove(x)
-    else:
-      print(f"Element {x} not found in Set.")
+    def union(self, other):
+        result = []
+        for item in self.a:
+            result.append(item)
+          
+        for item in other.a:
+            duplicate = False
+            for res_item in result:
+                if res_item == item:
+                    duplicate = True
+                    break
+            if not duplicate:
+                result.append(item)
+        self.a = result
+      
 
-  def union(self,x):
-    self.a = self.a | x.a
+    def intersection(self, other):
+        result = []
+        for item in self.a:
+            for other_item in other.a:
+                if item == other_item:
+                    result.append(item)
+                    break
+        self.a = result
 
-  def intersection(self,x):
-    self.a = self.a & x.a
+    def diff(self, other):
+        result = []
+        for item in self.a:
+            found = False
+            for other_item in other.a:
+                if item == other_item:
+                    found = True
+                    break
+            if not found:
+                result.append(item)
+        self.a = result
 
-  def diff(self,x):
-    self.a = self.a - x.a
+    def display(self):
+        return self.a
 
-  def display(self):
-    return self.a
+    def isSubset(self, other):
+        for item in self.a:
+            found = False
+            for other_item in other.a:
+                if item == other_item:
+                    found = True
+                    break
+            if not found:
+                return False
+        return True
 
-  def isSubset(self,x):
-    return self.a.issubset(x.a)
+    def search(self, x):
+        for item in self.a:
+            if item == x:
+                return True
+        return False
 
-  def search(self,x):
-    return x in self.a
-
-  def size(self):
-    return len(self.a)
+    def size(self):
+        count = 0
+        for i in self.a:
+            count += 1
+        return count
+      
